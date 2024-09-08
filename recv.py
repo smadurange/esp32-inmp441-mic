daae9792902352274910e9b9bc314b532de18f11
Sadeep Madurange <sadeep@asciimx.com>
2024-09-08 14:47:36 +0800

    Python UDP server.

---
+++ recv.py
@@ -0,0 +1,21 @@
+import datetime
+import wave
+import sys
+import socket
+
+read_until = datetime.datetime.now() + datetime.timedelta(seconds = 30)
+
+wav = wave.open('sound.wav', 'wb')
+wav.setnchannels(1)
+wav.setframerate(8000)
+wav.setsampwidth(4)
+
+try:
+	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	sock.bind(('', 12000))
+
+	while datetime.datetime.now() < read_until:
+    	msg, _ = sock.recvfrom(1024)
+		wav.writeframes(msg)
+finally:
+	wav.close()
